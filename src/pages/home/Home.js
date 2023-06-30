2ca4b6b5b1025b5be085df13bd91913787b409ec
tonytovan <tonytovan@gmail.com>
2023-06-30 10:16:47 +0200

    Eindopdracht project opgezet

---
+++ src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Home.css'
+
+
+function Home() {
+    return (
+        <body>
+        <header className="header-section">
+            <h1>Lil' Chef</h1>
+            <p>Yo! </p>
+        </header>
+        <main>
+        <section>
+            <h2>Let's start cooking! </h2>
+        </section>
+        </main>
+
+        <footer>
+
+        </footer>
+
+        </body>
+    );
+}
+
+export default Home;
